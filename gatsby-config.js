848ec1df8f86134819c87f97041dfcf855da7c99
ryjpeterson <ryan.j.peterson.80@gmail.com>
2020-09-04 11:45:22 -0500

    favicon setup with gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,33 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portfolio of RJP`,
+        short_name: `Portfolio`,
+        description: `Portfolio site displaying work`,
+        lang: `en`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon-16x16.png`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-sharp`,
+    //   options: {
+    //     // Available options and their defaults:
+    //     icon: `src/images/favicon-32x32.png`,
+    //     base64Width: 20,
+    //     forceBase64Format: `png`, // valid formats: png,jpg,webp
+    //     useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+    //     stripMetadata: true,
+    //     defaultQuality: 50,
+    //   },
+    // },
+  ]
 }
